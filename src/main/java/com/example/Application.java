f0aff0f6c99d86e26cbf047ec63f0c06d22467c5
Erik Östlund <erik.ostlund@gmail.com>
2020-09-20 10:06:29 +0200

    First commit

---
+++ src/main/java/com/example/Application.java
@@ -0,0 +1,47 @@
+package com.example;
+
+import cloud.piranha.nano.NanoPiranhaBuilder;
+import cloud.piranha.nano.NanoRequestBuilder;
+import cloud.piranha.nano.NanoResponseBuilder;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Application {
+
+    public static void main(String[] args) throws IOException, ServletException {
+        var servlet = new HttpServlet() {
+            @Override
+            protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+                response.setContentType("text/html");
+                response.getWriter().println("""
+                                     <html>
+                                       <body>
+                                         Hello world!
+                                       </body>
+                                     </html>
+                                     """);
+                response.getWriter().close();
+            }
+        };
+
+        var pirahna = new NanoPiranhaBuilder()
+                .servlet("HelloWorldServlet", servlet)
+                .build();
+
+
+        var request = new NanoRequestBuilder().build();
+        var outputStream = new ByteArrayOutputStream();
+        var response = new NanoResponseBuilder()
+                .outputStream(outputStream)
+                .build();
+
+        pirahna.service(request, response);
+
+        System.out.println("Response content type: " + response.getContentType());
+        System.out.println("Response body: " + System.lineSeparator() + outputStream.toString());
+    }
+}
